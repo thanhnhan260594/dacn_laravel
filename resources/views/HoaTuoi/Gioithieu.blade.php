480416b9a125d8a291972f6ff0ebee1aadec704c
thanhnhan260594 <thanhnhan260594@gmail.com>
2016-11-22 17:18:38 +0700

    gioi thieu

---
+++ resources/views/HoaTuoi/Gioithieu.blade.php
@@ -0,0 +1,59 @@
+@extends('Shared.LayoutUser')
+
+@section('content')
+<h2>
+    <img src="{{asset("resources/views/Themes/Images/icon.png")}}" width="30" height="30" />
+     GIỚI THIỆU
+</h2>
+<div class="main_news row">
+	<div class="border intro">
+
+	<p style="text-align: justify;">
+	Từ ngàn xưa, dẫu cuộc sống nghèo khó, cha ông ta cũng đã biết thưởng ngoạn vẻ đẹp món quà đặc biệt của thiên nhiên, sự tinh túy của đất trời, làm phong phú thêm vẻ đẹp tâm hồn của con người, đó là hương và sắc của ngàn hoa rực rỡ!
+	</p>
+
+	<p style="text-align: justify;">
+	Ngày nay, nhịp sống hối hả có thể khiến nhiều người quên đi thú vui bình dị mà thanh tao, giản đơn mà cao quý ấy. Nhưng, cùng với sự thành đạt của mỗi người, sự tiến bộ của xã hội, hương sắc của hoa dần trở thành một phần tất yếu của cuộc sống.</p>
+
+	<p style="text-align: justify;">
+	Không chỉ là vật trang trí cho không gian được sang trọng, hoa còn trở thành công cụ thể hiện đẳng cấp và phong cách sống khác biệt, là phương tiện để sẻ chia những niềm vui nỗi buồn, và đặc biệt, hoa trở thành phương tiện để giao tế một cách hiệu quả khó có thể thay thế.
+	</p>
+
+	<p style="text-align: justify;">
+	Cuộc sống và công việc của bạn dù có bộn bề với những lo toan, hãy đến với Hoa tươi H & T, chúng tôi sẽ giúp bạn bắc nhịp cầu nối những bờ vui, làm vững bền mối thâm giao bằng hữu, giúp bạn tạo dựng quan hệ thân tình, tạo dựng đẳng cấp và phong cách khác biệt trong cuộc sống và công việc hay để chia ngọt sẻ bùi, gửi gắm tâm tư ...</p>
+
+
+	<p style="text-align: justify;">
+	Chỉ đơn giản bằng một mẩu tin nhắn hay một cuộc điện thoại, mọi nhu cầu về hoa của bạn sẽ được Hoa tươi H & T đáp ứng tận nơi!</p>
+	
+    <center>
+    	<img  src="{{asset("resources/views/Themes/Images/nhanvien01.jpg")}}" width="auto"  height="500" />
+    	<p><i> Hình ảnh nhân viên của cửa hàng </i></p>
+    </center>  
+
+	<div>
+		<strong>Những Cam kết của Hoa tươi H & T :</strong>
+	</div>
+	<div>&nbsp;</div>
+	<div>
+		Hoa tươi H & T luôn cam kết chất lượng hoa cao nhất cho khách hàng, hoa luôn tươi mới, đẹp và tuyển nhất. Các mẫu hoa luôn được cắm đủ số lượng và giống kiểu dáng trên website nhất có thể
+	</div>
+	<div>&nbsp;</div>
+	<div>
+		Là dịch vụ hoa tươi lớn và chuyên nghiệp nhất tại TPHCM, là khách hàng thân thiết của các kiều bào Mỹ, Úc, Canada, Châu Âu. Hoa tươi H & T luôn đổi mới không ngừng và cải tiến các dịch vụ tốt hơn để luôn làm hài lòng khách hàng.
+	</div>
+	<div>&nbsp;</div>
+	<div>
+		Sử dụng các vật liệu thân thiện môi trường nhất có thể, các vật liệu có thể tái chế, các vật liệu cho Việt Nam sản xuất.
+	</div>
+	<div>&nbsp;</div>
+	<p style="text-align: justify;">
+		<strong>Trao trọn thành ý, vun đắp tình thân</strong>, đó là sự khác biệt cơ bản mà bạn sẽ đạt được khi sử dụng sản phẩm và dịch vụ của Hoa tươi H & T. Với phương châm tất cả vì sự hài lòng của khách hàng, Hoa tươi H & T cam kết mang đến bạn những sản phẩm tinh tế và hoàn hảo nhất, ý nghĩa nhất với giá cả phù hợp nhất cùng với chế độ hậu mãi chu đáo, thái độ phục vụ tận tụy và chuyên nghiệp.</p>
+		<p>
+			Trân trọng.</p>
+		
+
+		</div>
+	</div>
+</div>
+@endsection
